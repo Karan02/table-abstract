535ffa35a34f45c9d1556967618eee97295a1788
Karan02 <krnptl1234@gmail.com>
2019-11-29 20:48:38 +0530

    related to filter changes
    related to filter changes

--- src/columns.js
+++ src/columns.js
@@ -1,38 +1,31 @@
 const columns = [
   {
-    title:"roll_no",
-    sorter:true,
-    width:"250px",
+    title: "roll_no",
+    sorter: true,
+    width: "250px"
   },
   {
-    title: 'name',
-    dataIndex: 'name',
-    width: '250px',
+    title: "name",
+    dataIndex: "name",
+    width: "250px",
     search: true,
     sorter: true,
-    filters: [{text:'Edward King 2'},{text:'Edward King 5'}]
+    filters: [{ text: "Edward King 2" }, { text: "Edward King 5" }]
   },
   {
-    title: 'gender',
-    dataIndex: 'gender',
-    filters: [{text:'Male'} ,{ text:'Female' }],
-    width: '200px',
-    search:true,
-    sorter:true,
-  },
-  {
-    title: 'address',
-    dataIndex: 'address',
+    title: "gender",
+    dataIndex: "gender",
+    filters: [{ text: "Male" }, { text: "Female" }],
+    width: "200px",
     search: true,
-    filters: [{text:'London'}, {text:'London'}],
-    width:"350px",
-    
-      
+    sorter: true
   },
   {
-    title:"location",
-    search:true,
-    width:"150px",
-  },
+    title: "address",
+    dataIndex: "address",
+    search: true,
+    filters: [{ text: "London" }, { text: "London" }],
+    width: "350px"
+  }
 ];
 export default columns;
--- src/components/table.js
+++ src/components/table.js
@@ -1,226 +1,254 @@
-import React from "react"
-import Header from "./header"
-import Content from "./content"
-
+import React from "react";
+import Header from "./header";
+import Content from "./content";
 
 class Table extends React.Component {
-   constructor(props) {
-       super(props);
-       this.state = {
-        showAddSection: false,
-        exampleItemsMaster: this.props.data,
-        exampleItems:this.props.data,  
-        pageOfItems: [],  
-        addContent:{},
-        blur:false,
-        arrayOk:[],
-        arrayFilter:[],
-        intermediateArray2:[],
-       }
-     this.handleSearchOk=this.handleSearchOk.bind(this)
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAddSection: false,
+      exampleItemsMaster: this.props.data,
+      exampleItems: this.props.data,
+      pageOfItems: [],
+      addContent: {},
+      blur: false,
+      arrayOk: [],
+      arrayFilter: [],
+      intermediateArray2: []
+    };
+    this.handleSearchOk = this.handleSearchOk.bind(this);
+  }
 
-    
-    showAddSection = () => {
-        this.setState({ 
-          showAddSection:!this.state.showAddSection
-        })
-    }
+  showAddSection = () => {
+    this.setState({
+      showAddSection: !this.state.showAddSection
+    });
+  };
 
-    handleChange = (e) => {
-      e.preventDefault();
-      let addContent = this.state.addContent
-      addContent[e.target.name] =e.target.value 
-       this.setState({addContent:addContent})  
-    }
-    
-    handleSubmit = (e) => {
-      e.preventDefault();
-      // console.log(this.state.addContent)
-      this.AddEntries(this.state.addContent)
-      this.setState({addContent:{},showAddSection:!this.state.showAddSection})
-    }
+  handleChange = e => {
+    e.preventDefault();
+    let addContent = this.state.addContent;
+    addContent[e.target.name] = e.target.value;
+    this.setState({ addContent: addContent });
+  };
 
-    AddEntries = (addContent) => {
-    
-      var newObject2={}
-      for(var i=0;i<this.props.columns.length;i++){
-        newObject2[this.props.columns[i].title]=addContent[this.props.columns[i].title];
-      }
-      let dataAdded = [...this.state.exampleItems]
-      dataAdded.push(newObject2)
-      this.setState({
-        exampleItems:dataAdded
-      })  
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    // console.log(this.state.addContent)
+    this.AddEntries(this.state.addContent);
+    this.setState({
+      addContent: {},
+      showAddSection: !this.state.showAddSection
+    });
+  };
 
-  handleAdd = () => { 
-      let addContent = this.state.addContent;
-      let handleChange = this.handleChange
-      let inputs = this.props.columns.map(function(column,index){   
-          return (<span id={index}>{column.title}:<input name={column.title} onChange={handleChange} value={addContent[column.title]} type="text" required="required" /></span>)
+  AddEntries = addContent => {
+    var newObject2 = {};
+    for (var i = 0; i < this.props.columns.length; i++) {
+      newObject2[this.props.columns[i].title] =
+        addContent[this.props.columns[i].title];
+    }
+    let dataAdded = [...this.state.exampleItems];
+    dataAdded.push(newObject2);
+    this.setState({
+      exampleItems: dataAdded
+    });
+  };
+
+  handleAdd = () => {
+    let addContent = this.state.addContent;
+    let handleChange = this.handleChange;
+    let inputs = this.props.columns.map(function(column, index) {
+      return (
+        <span id={index}>
+          {column.title}:
+          <input
+            name={column.title}
+            onChange={handleChange}
+            value={addContent[column.title]}
+            type="text"
+            required="required"
+          />
+        </span>
+      );
+    });
+    inputs[`${this.props.columns.length}`] = (
+      <button type="submit" className="SubmitButton">
+        Submit
+      </button>
+    );
+    return <form onSubmit={this.handleSubmit}>{inputs}</form>;
+  };
+
+  handleDelete = row => {
+    let exampleItems2 = [...this.state.exampleItems];
+    let index = exampleItems2.indexOf(row);
+    exampleItems2.splice(index, 1);
+    this.setState({
+      exampleItems: exampleItems2
+    });
+  };
+
+  onProductTableUpdate = evt => {
+    let item = {
+      id: evt.target.id,
+      name: evt.target.name,
+      value: evt.target.value
+    };
+    console.log(item);
+    let products = [...this.state.exampleItems];
+    let newProducts = products.map(function(product, index) {
+      Object.keys(product).forEach(key => {
+        if (key == item.name && index + 1 == item.id) {
+          product[key] = item.value;
         }
-      ) 
-      inputs[`${this.props.columns.length}`]= <button type="submit" className="SubmitButton" >Submit</button>
-      return <form onSubmit={this.handleSubmit}>{inputs}</form>
-  }
-
-  handleDelete = (row) => {
-      let  exampleItems2  = [...this.state.exampleItems];
-      let index = exampleItems2.indexOf(row);       
-      exampleItems2.splice(index,1)
-      this.setState({         
-        exampleItems:exampleItems2
-      });      
-  }
-
-  onProductTableUpdate = (evt) => {
-        let item = {
-          id: evt.target.id,
-          name: evt.target.name,
-          value: evt.target.value
-        };
-        console.log(item)
-        let products = [...this.state.exampleItems];
-        let newProducts = products.map(function(product,index) {
-          
-          Object.keys(product).forEach(key => {
-           if (key == item.name && (index+1) == item.id) {      
-                   product[key] = item.value;    
-                 }                 
-          }) 
-          return product;
-        });
-        console.log(newProducts)
-        this.setState({exampleItems:newProducts});
-        
-    }
+      });
+      return product;
+    });
+    console.log(newProducts);
+    this.setState({ exampleItems: newProducts });
+  };
 
   handleAddSection = () => {
-       
-        if(this.state.showAddSection){
-        return this.handleAdd();}
+    if (this.state.showAddSection) {
+      return this.handleAdd();
     }
-     
-  handleSort = (order,index) => {
-      var arrangeMethod = order
-      this.orderChange(index,arrangeMethod)
-    } 
+  };
 
-  orderChange = (index,arrangeMethod) =>{
-      
-      this.arrangeText(index,arrangeMethod);
-      
-    }
+  handleSort = (order, index) => {
+    var arrangeMethod = order;
+    this.orderChange(index, arrangeMethod);
+  };
 
-  arrangeText = (key,arrangeMethod) => {
-      
-        let arraycopy = [...this.state.exampleItems];
-        if(arrangeMethod === "asc"){
-          arraycopy.sort(function(a,b){
-            if((a[Object.keys(a)[key]]) < (b[Object.keys(b)[key]])){ return -1;}
-            if((a[Object.keys(a)[key]]) > (b[Object.keys(b)[key]])){ return 1;}
-            return 0
-          })
+  orderChange = (index, arrangeMethod) => {
+    this.arrangeText(index, arrangeMethod);
+  };
+
+  arrangeText = (key, arrangeMethod) => {
+    let arraycopy = [...this.state.exampleItems];
+    if (arrangeMethod === "asc") {
+      arraycopy.sort(function(a, b) {
+        if (a[Object.keys(a)[key]] < b[Object.keys(b)[key]]) {
+          return -1;
         }
-        if(arrangeMethod === "des"){
-          arraycopy.sort(function(a,b){
-            if((a[Object.keys(a)[key]]) > (b[Object.keys(b)[key]])){ return -1;}
-            if((a[Object.keys(a)[key]]) < (b[Object.keys(b)[key]])){ return 1;}
-            return 0
-          })
+        if (a[Object.keys(a)[key]] > b[Object.keys(b)[key]]) {
+          return 1;
         }
-      
-        this.setState({exampleItems:arraycopy});
+        return 0;
+      });
+    }
+    if (arrangeMethod === "des") {
+      arraycopy.sort(function(a, b) {
+        if (a[Object.keys(a)[key]] > b[Object.keys(b)[key]]) {
+          return -1;
+        }
+        if (a[Object.keys(a)[key]] < b[Object.keys(b)[key]]) {
+          return 1;
+        }
+        return 0;
+      });
     }
 
-  fireblur = (boole) => {
-      this.setState({blur:boole})
-    } 
-   
-  
-  handleSearchOk = (arrayOk,resetIndex) => {
-    this.setState({arrayOk:arrayOk})
-  } 
-  
-  handleFilterMaster = (matrix,index) => {
-    this.setState({arrayFilter:matrix})
-  }
+    this.setState({ exampleItems: arraycopy });
+  };
 
-  render(){
+  fireblur = boole => {
+    this.setState({ blur: boole });
+  };
 
-    let handleArray = this.state.exampleItems 
+  handleSearchOk = (arrayOk, resetIndex) => {
+    this.setState({ arrayOk: arrayOk });
+  };
+
+  handleFilterMaster = (matrix, index) => {
+    this.setState({ arrayFilter: matrix });
+  };
+
+  render() {
+    let handleArray = this.state.exampleItems;
     for (let i = 0; i < this.state.arrayOk.length; i++) {
-      if(this.state.arrayOk[i] && this.state.arrayOk[i]!=="") {
-        handleArray = handleArray.filter(item => { 
-          return item[Object.keys(item)[i]].indexOf(this.state.arrayOk[i]) !== -1
+      if (this.state.arrayOk[i] && this.state.arrayOk[i] !== "") {
+        handleArray = handleArray.filter(item => {
+          return (
+            item[Object.keys(item)[i]].indexOf(this.state.arrayOk[i]) !== -1
+          );
         });
       }
     }
 
-    let pushArray = handleArray
-    let intermediateArray = []
-    let intermediateArray2 = []
-    this.state.arrayFilter.forEach((element,index) => {
-        element.forEach((each,indexinner) => {  
-          if(each.isActive){  
-            intermediateArray = pushArray.filter(row => {  
-              return(
-                (row[Object.keys(row)[index]]).indexOf(each.text) !== -1
-              );
-            })       
-            intermediateArray2=intermediateArray2.concat(intermediateArray)
-          }
-        });
-    });
+    let pushArray = handleArray;
+    let intermediateArray = [];
+    let intermediateArray2 = [];
+    let intermediateArray3 = [];
+    let intermediateArray3bool = false;
+    this.state.arrayFilter.forEach((element, index) => {
+      if (element) {
+        element.forEach((each, indexinner) => {
+          if (each) {
+            if (each.isActive) {
+              console.log(pushArray, "push array");
+              intermediateArray = pushArray.filter(row => {
+                return row[Object.keys(row)[index]].indexOf(each.text) !== -1;
+              });
+              intermediateArray2 = intermediateArray2.concat(intermediateArray);
+              console.log("intermediate array", intermediateArray);
+            }
+          }
+        });
+        intermediateArray3bool = true;
+        // continue your work here
+        if (intermediateArray2.length) {
+          console.log("1");
+          pushArray = intermediateArray2;
+          intermediateArray3 = intermediateArray2;
+          intermediateArray2 = [];
+        }
+      }
+    });
 
-   if(!intermediateArray2){
-    // continue your work here
-   }
+    return (
+      <div>
+        <div className={this.state.blur ? "blur" : null}>
+          <table>
+            <Header
+              columns={this.props.columns}
+              handleSort={this.handleSort}
+              handleSearchOk={this.handleSearchOk}
+              handleSearchReset={this.handleSearchReset}
+              handleFilterMaster={this.handleFilterMaster}
+            />
 
-    return(
-        <div>
-        <div  className={this.state.blur ? "blur":null}>
-            <table>
-            
-              <Header 
-                columns={this.props.columns} 
-                handleSort={this.handleSort}
-                handleSearchOk={this.handleSearchOk}
-                handleSearchReset={this.handleSearchReset}
-                handleFilterMaster={this.handleFilterMaster}
-              />
-             
-                
-              {intermediateArray2.length ? 
-              <Content 
+            {intermediateArray3.length && intermediateArray3bool ? (
+              <Content
                 handleDelete={this.handleDelete}
                 columns={this.props.columns}
-                data={intermediateArray2}
+                data={intermediateArray3}
                 onProductTableUpdate={this.onProductTableUpdate}
                 fireblur={this.fireblur}
-              />:
-              <Content 
+              />
+            ) : (
+              <Content
                 handleDelete={this.handleDelete}
                 columns={this.props.columns}
-                data={pushArray}
+                data={handleArray}
                 // currentIndex={this.state.topIndex}
                 onProductTableUpdate={this.onProductTableUpdate}
                 fireblur={this.fireblur}
                 startIndex={this.startIndex}
-              />}
+              />
+            )}
+          </table>
 
-            </table>
-            
-            <div className="addInput">
-              <button className="addButton" onClick={this.showAddSection}>Add</button><br />
-                <div className="inputs">
-                  {this.handleAddSection()}
-                </div>
-            </div>
+          <div className="addInput">
+            <button className="addButton" onClick={this.showAddSection}>
+              Add
+            </button>
+            <br />
+            <div className="inputs">{this.handleAddSection()}</div>
+          </div>
         </div>
-  </div>
-  );
+      </div>
+    );
   }
 }
-export default Table
+export default Table;
